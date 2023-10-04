97569459c8c99fbc8738a79743cd2cf0e48a88bc
Polina Kostsova <kostsovapa@gmail.com>
2023-10-04 20:53:41 +0300

    Third task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿// 
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран:
+int[] randomArray = new int[8];
+for (int i = 0; i < 8; i++)
+{
+randomArray[i] = new Random().Next(1,99);
+Console.Write(randomArray[i] + ",");
+}
